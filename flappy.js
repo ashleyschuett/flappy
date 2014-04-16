7b3689c5c3af674d7962cb69eb9fa539a074f184
Ashley <schuetta@uwm.edu>
2014-04-15 22:41:27 -0500

    Consolidated code.

--- flappy.js
+++ flappy.js
@@ -4,34 +4,27 @@ var keys = [];
 
 var playing = true;
 
-var pipe1 = {
+var pipes = [{
 		x: $('#canvas').width(),
 		y: 0,
-	};
-
-var pipe2 = {
+	},{
 		x: $('#canvas').width() + 275,
 		y: 0,
-	};
-var pipe3 = {
+	}, {
 		x: $('#canvas').width() + 550,
 		y: 0,
-	};
+	}];
 
-var cutout1 = {
+var cutouts = [{
         x: $('#canvas').width(),
         y: Math.random() * 250 + 25,
-    };
-
-var cutout2 = {
+    }, {
         x: $('#canvas').width() + 275,
         y: Math.random() * 250 + 25,
-    };
-
-var cutout3 = {
+    }, {
         x: $('#canvas').width() + 550,
         y: Math.random() * 250 + 25,
-    };
+    }];
 
 var boxy = {
     x: 225,
@@ -45,45 +38,25 @@ var dummy = {
     g: 0.2
 }
 
-function pipe(){
-	return  pipe = {
-		x: $('#canvas').width(),
-		y: 0,
-	};
-}
-
 function render(){
 	ctx.fillStyle = '#ffffff';
     ctx.fillRect(0, 0, $('#canvas').width(), $('#canvas').height());
     
     ctx.fillStyle = '#33ffdd';
 
-
-    //Pipe Number one
-   	ctx.fillRect(pipe1.x, 0, 50, $('#canvas').height() );
-    ctx.clearRect(cutout1.x, cutout1.y, 50, 150);
-    if(pipe1.x < 0){
-    	pipe1.x = $('#canvas').width();
-        cutout1.x = $('#canvas').width();
-        cutout1.y =  Math.random() * 250 + 25;
+    for(var i in pipes){
+     	ctx.fillRect(pipes[i].x, 0, 50, $('#canvas').height() );
+      if(pipes[i].x < 0){
+      	pipes[i].x = $('#canvas').width();
+      }
     }
 
-	//Pipe Number 2
-    ctx.fillRect(pipe2.x, 0, 50, $('#canvas').height() );
-    ctx.clearRect(cutout2.x, cutout2.y, 50, 150);
-    if(pipe2.x < 0){
-    	pipe2.x = $('#canvas').width();
-        cutout2.x = $('#canvas').width();
-        cutout2.y = Math.random() * 250 + 25;
-    }
-
-    //Pipe # 3
-    ctx.fillRect(pipe3.x, 0, 50, $('#canvas').height() );
-    ctx.clearRect(cutout3.x, cutout3.y, 50, 150);
-    if(pipe3.x < 0){
-    	pipe3.x = $('#canvas').width();
-        cutout3.x = $('#canvas').width();
-        cutout3.y = Math.random() * 250 + 25;
+    for(var i in cutouts){
+      ctx.clearRect(cutouts[i].x, cutouts[i].y, 50, 150);
+      if(cutouts[i].x < 0){
+          cutouts[i].x = $('#canvas').width();
+          cutouts[i].y =  Math.random() * 250 + 25;
+      }
     }
 
     var cap = Math.min(Math.max(boxy.g - 2, 0), 12);
@@ -97,44 +70,19 @@ function render(){
     ctx.fillStyle = '#000000';
     ctx.fillRect( 0, 0, 11, 2); 
     ctx.restore();
-
-    ctx.beginPath();
-      ctx.moveTo(0, 0);
-      ctx.lineTo(0, 450);
-      ctx.lineWidth = 10;
-      ctx.stroke();
-
-      ctx.beginPath();
-      ctx.moveTo(0, 0);
-      ctx.lineTo(800, 0);
-      ctx.lineWidth = 10;
-      ctx.stroke();
-
-      ctx.beginPath();
-      ctx.moveTo(0, 450);
-      ctx.lineTo(800, 450);
-      ctx.lineWidth = 10;
-      ctx.stroke();
-
-      ctx.beginPath();
-      ctx.moveTo(800, 0);
-      ctx.lineTo(800, 450);
-      ctx.lineWidth = 10;
-      ctx.stroke();
  
 }
 
 function update(){
 
     //update pipe
-    pipe1.x += -3;
-    pipe2.x += -3;
-    pipe3.x += -3;
+    for(var i in pipes)
+       pipes[i].x += -3;
 
     //update cutouts
-    cutout1.x += -3;
-    cutout2.x += -3;
-    cutout3.x += -3;
+    for(var i in cutouts)
+       cutouts[i].x += -3;
+   
 
     if (keys[32]) {
         keys[32] = false;
@@ -156,24 +104,12 @@ function update(){
 }
 
 function collision(){
-
-    if (boxy.x >= cutout1.x && boxy.x <= cutout1.x+50 && boxy.y <= cutout1.y)
-        playing =false;
-    else if(boxy.x >= cutout1.x && boxy.x <= cutout1.x+50 && boxy.y >= cutout1.y+150)
-        playing = false;
-
-     if (boxy.x >= cutout2.x && boxy.x <= cutout2.x+50 && boxy.y <= cutout2.y)
-        playing =false;
-    else if(boxy.x >= cutout2.x && boxy.x <= cutout2.x+50 && boxy.y >= cutout2.y+150)
-        playing = false;
-
-    if (boxy.x >= cutout3.x && boxy.x <= cutout3.x+50 && boxy.y <= cutout3.y)
-        playing =false;
-    else if(boxy.x >= cutout3.x && boxy.x <= cutout3.x+50 && boxy.y >= cutout3.y+150)
-        playing = false;
-
-
-    console.log(boxy.x  + " " + cutout1.x);
+    for(var i in cutouts){
+        if (boxy.x >= cutouts[i].x && boxy.x <= cutouts[i].x+50 && boxy.y <= cutouts[i].y)
+            playing =false;
+        else if(boxy.x >= cutouts[i].x && boxy.x <= cutouts[i].x+50 && boxy.y >= cutouts[i].y+150)
+            playing = false;
+    }
 }
 
 function jumping(){
@@ -206,8 +142,8 @@ $(document).keyup(function(e) {
 
 //function to declare how often to run the other functions
 setInterval(function() {
-    if(playing){
+  if(playing){
     update();
     render();
-    }
+  }
 }, 20);
